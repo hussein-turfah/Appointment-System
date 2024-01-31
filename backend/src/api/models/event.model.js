730f0a1f77991f01fa8a7704fa8ca98e44ed41c6
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-01 01:16:26 +0200

    :boom: Add new event models

---
+++ backend/src/api/models/event.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const httpStatus = require("http-status");
+
+/**
+ * Event Schema
+ * @private
+ */
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    patientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    secretaryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * @typedef Event
+ */
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
