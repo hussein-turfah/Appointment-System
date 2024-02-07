f92c60806501f28152cbbb9b8309176e8a368b91
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-07 11:54:24 +0200

    :boom: Created new components

--- frontend/pages/App.js
+++ frontend/pages/App.js
@@ -1,11 +1,24 @@
-import { AnimatePresence } from "framer-motion";
-
+import { AnimatePresence, motion } from "framer-motion";
+import Navbar from "../common/Navbar";
 
 const MyApp = ({ Component, pageProps, domainName }) => {
   return (
     <div className="page">
       <AnimatePresence mode="wait">
-        <Component key={"pageContent"} {...pageProps} domainName={domainName} />
+        <Navbar />
+        <motion.div
+          initial={{ y: 400 }}
+          exit={{ y: 400 }}
+          animate={{ y: 0 }}
+          transition={{ bounce: 0, type: "spring", duration: 0.8 }}
+          className="pageContent"
+        >
+          <Component
+            key={"pageContent"}
+            {...pageProps}
+            domainName={domainName}
+          />
+        </motion.div>
       </AnimatePresence>
     </div>
   );
