6f6daa7d75e38618180db0155110bc79860e9490
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-07 11:54:33 +0200

    :boom: Created new components

--- frontend/pages/_app.page.js
+++ frontend/pages/_app.page.js
@@ -4,6 +4,8 @@ import App from "./App";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import "../styles/globals.scss";
+import "../styles/fonts.scss";
 
 config.autoAddCss = false;
 
