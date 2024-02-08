ac411bca0afa22ffb0da7fdb264cff6799ee69bc
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 00:31:42 +0200

    :boom: Added several keys

--- backend/src/api/models/scheduleSchema.js
+++ backend/src/api/models/scheduleSchema.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const doctorScheduleSchema = new mongoose.Schema({
-  doctor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Doctor',
-    required: true
+const doctorScheduleSchema = new mongoose.Schema(
+  {
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    weekday: {
+      type: String,
+      enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      required: true,
+    },
+    startTime: {
+      type: String,
+      match: /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/, // Validate time format
+      required: true,
+    },
+    endTime: {
+      type: String,
+      match: /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/,
+      required: true,
+    },
   },
-  weekday: {
-    type: String,
-    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-    required: true
-  },
-  startTime: {
-    type: String,
-    match: /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/, // Validate time format
-    required: true
-  },
-  endTime: {
-    type: String,
-    match: /^([0-1][0-9]|2[0-3]):[0-5][0-9]$/,
-    required: true
+  {
+    timestamps: true,
   }
-});
+);
 
-module.exports = mongoose.model('DoctorSchedule', doctorScheduleSchema);
+module.exports = mongoose.model("DoctorSchedule", doctorScheduleSchema);
