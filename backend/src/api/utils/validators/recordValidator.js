2ec4e3ac67382982ca9a5af4ea6905baddb6ce16
Zeinab <zeinabhmohsen@gmail.com>
2024-02-08 18:53:34 +0200

    records

---
+++ backend/src/api/utils/validators/recordValidator.js
@@ -0,0 +1,64 @@
+const { check } = require('express-validator');
+const validatorMiddleware = require('../../middleware/validatorMiddleware');
+const MedicalRecord = require('../../models/medicalRecords');
+
+// Validator middleware for creating a medical record
+exports.createMedicalRecordValidator = [
+  check('patientId')
+    .notEmpty()
+    .withMessage('Patient ID is required'),
+  check('title')
+    .notEmpty()
+    .withMessage('Title is required'),
+  check('notes')
+    .notEmpty()
+    .withMessage('Notes are required'),
+  check('doctorId')
+    .notEmpty()
+    .withMessage('Doctor ID is required'),
+  validatorMiddleware,
+];
+
+// Validator middleware for updating a medical record
+exports.updateMedicalRecordValidator = [
+  check('medicalRecordId')
+    .isMongoId()
+    .withMessage('Invalid medical record ID format'),
+  check('title')
+    .optional()
+    .notEmpty()
+    .withMessage('Title is required'),
+  check('notes')
+    .optional()
+    .notEmpty()
+    .withMessage('Notes are required'),
+  check('doctorId')
+    .optional()
+    .notEmpty()
+    .withMessage('Doctor ID is required'),
+  validatorMiddleware,
+];
+
+
+exports.deleteMedicalRecordValidator = [
+  check('medicalRecordId')
+    .isMongoId()
+    .withMessage('Invalid medical record ID format'),
+  validatorMiddleware,
+];
+
+
+exports.getMedicalRecordByIdValidator = [
+  check('medicalRecordId')
+    .isMongoId()
+    .withMessage('Invalid medical record ID format'),
+  validatorMiddleware,
+];
+
+
+exports.getMedicalRecordByPatientIdValidator = [
+  check('patientId')
+    .isMongoId()
+    .withMessage('Invalid patient ID format'),
+  validatorMiddleware,
+];
