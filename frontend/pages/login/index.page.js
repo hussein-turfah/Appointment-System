93a721344a72c04326372bfb1e31482a3768b371
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 17:15:07 +0200

    :boom: Add new elements

--- frontend/pages/login/index.page.js
+++ frontend/pages/login/index.page.js
@@ -1,19 +1,5 @@
-import { useRouter } from "next/router";
-import styles from "./styles/index.module.scss";
 import SignInForm from "./signInForm";
 
 export default function Login() {
-
-
-  return (
-    // <main className={styles.main}>
-    //   <h1 className={styles.title}>Welcome to my Next.js app!</h1>
-    //   <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1>
-    //   <button onClick={googleAuth}>Login with Google</button>
-    // </main>
-    <SignInForm />
-  );
-};
-
+  return <SignInForm />;
+}
