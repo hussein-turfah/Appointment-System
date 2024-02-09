f6c361285d804d1c9516016553b19a72cfaeefd2
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 17:03:26 +0200

    :boom: Add new actions with reducers

--- frontend/pages/App.js
+++ frontend/pages/App.js
@@ -1,17 +1,41 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "../common/Navbar";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getAllDoctors } from "../actions/DoctorActions";
 
 const MyApp = ({ Component, pageProps, domainName }) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const authPages = ["/login", "/register", "/forgot-password"];
+  const isAuthPage = authPages.includes(router.pathname);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token && !isAuthPage) {
+      router.push("/login");
+    }
+  }, []);
+
+  useEffect(() => {
+    // get all doctors
+    dispatch(getAllDoctors());
+  }
+  , []);
+
+
   return (
     <div className="page">
       <AnimatePresence mode="wait">
-        <Navbar />
+        {!isAuthPage && <Navbar />}
         <motion.div
           initial={{ y: 400 }}
           exit={{ y: 400 }}
           animate={{ y: 0 }}
           transition={{ bounce: 0, type: "spring", duration: 0.8 }}
           className="pageContent"
+          style={{ top: !isAuthPage ? "3.5rem" : "0" }}
         >
           <Component
             key={"pageContent"}
