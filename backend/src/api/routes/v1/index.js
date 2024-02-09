497d84d390a668bb834c3424adb787c5ed22389b
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 13:53:00 +0200

    :wrench: Fix some issues

--- backend/src/api/routes/v1/index.js
+++ backend/src/api/routes/v1/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const { authLimiter } = require("../../middleware/rateLimiter");
 
-// const authRoutes = require("./auth.route");
-// const authRoute = require("./auth.route");
+const AuthRoute = require("./authRoutes")
 const patientRoute = require("./patientRoute");
 const doctorRoute = require("./doctorRoute");
 const RecordRoute = require("./recordRoutes");
 const PrescriptionsRoute = require("./prescriptionRoutes");
 const InvoiceRoute = require("./invoiceRoutes");
 const SecretaryRoute = require("./secretaryRoute");
-const AuthRoute = require("./authRoutes")
 const appointmentRoutes = require("./appointment.route");
 const scheduleRoutes = require("./scheduleRoute");
 const app = express();
@@ -17,15 +14,14 @@ const app = express();
 const router = express.Router();
 
 router.get("/status", (req, res) => res.send("OK"));
-// router.use("/auth", authLimiter, authRoutes);
-// app.use("/auth", authRoute);
-app.use("/auth",AuthRoute)
-app.use("/patient", patientRoute);
-app.use("/doctor", doctorRoute);
-app.use("/record", RecordRoute);
-app.use("/prescription", PrescriptionsRoute);
-app.use("/invoices", InvoiceRoute);
-app.use("/secretary",SecretaryRoute);
+
+router.use("/auth",AuthRoute)
+router.use("/patient", patientRoute);
+router.use("/doctor", doctorRoute);
+router.use("/record", RecordRoute);
+router.use("/prescription", PrescriptionsRoute);
+router.use("/invoices", InvoiceRoute);
+router.use("/secretary",SecretaryRoute);
 router.use("/appointment", appointmentRoutes);
 router.use("/schedule", scheduleRoutes);
 
