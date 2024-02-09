b0cc46591c06e55a6b80ef9f34e64a42d107e37a
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 10:11:50 +0200

    :boom: Added several keys

---
+++ backend/src/api/routes/v1/appointment.route.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createAppointmentValidator,
+  getAppointmentValidator,
+  updateAppointmentValidator,
+  deleteAppointmentValidator,
+  getAppointmentByDoctorValidator,
+  getAppointmentByPatientValidator,
+  updateAppointmentStatusValidator,
+} = require("../../utils/validators/appointmentValidator");
+const {
+  getAppointmentById,
+  getAllAppointments,
+  createAppointment,
+  getAppointmentsByDoctorId,
+  getAppointmentByPatientId,
+  updateAppointmentStatus,
+  updateAppointment,
+  deleteAppointment,
+} = require("../../controllers/appointment.controller");
+
+router
+  .post("/", createAppointmentValidator, createAppointment)
+  .put("/:id", updateAppointmentValidator, updateAppointment);
+
+router
+  .get("/", getAllAppointments)
+  .get("/:id", getAppointmentValidator, getAppointmentById)
+  .delete("/:id", deleteAppointmentValidator, deleteAppointment);
+
+router
+  .get(
+    "/doctor/:doctor",
+    getAppointmentByDoctorValidator,
+    getAppointmentsByDoctorId
+  )
+  .get(
+    "/patient/:patient",
+    getAppointmentByPatientValidator,
+    getAppointmentByPatientId
+  );
+
+router.patch(
+  "/:id/:status",
+  updateAppointmentStatusValidator,
+  updateAppointmentStatus
+);
+
+module.exports = router;
