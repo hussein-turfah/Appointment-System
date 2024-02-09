12a162e215e2f01a7fc7bba29e70ff08b9e47643
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 13:20:09 +0200

    :boom: Added new functions

--- backend/src/api/routes/v1/index.js
+++ backend/src/api/routes/v1/index.js
@@ -25,8 +25,7 @@ app.use("/record", RecordRoute);
 app.use("/prescription", PrescriptionsRoute);
 app.use("/invoices", InvoiceRoute);
 app.use("/secretary",SecretaryRoute);
-
 router.use("/appointment", appointmentRoutes);
-// router.use("/schedule", scheduleRoutes);
+router.use("/schedule", scheduleRoutes);
 
 module.exports = router;
