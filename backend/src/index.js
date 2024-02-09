36c5810e01bc72fc38b03c3cbac6b59d8d7ec64f
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-09 10:12:38 +0200

    :boom: Added new functions

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
-const passportSetup = require("../passport");
-const authRoute = require("./api/routes/v1/auth.route");
-const patientRoute = require("./api/routes/v1/patientRoute");
-const doctorRoute = require("./api/routes/v1/doctorRoute");
-const ScheduleRoute = require("./api/routes/v1/scheduleRoute");
-const RecordRoute = require("./api/routes/v1/recordRoutes");
-const PrescriptionsRoute = require("./api/routes/v1/prescriptionRoutes");
-const InvoiceRoute = require("./api/routes/v1/invoiceRoutes")
-const app = express();
+const routes = require("./api/routes/v1/index");
 const session = require("express-session");
 const mongoose = require("./config/mongoose");
+const app = express();
 app.use(express.json());
 
 app.use(
@@ -39,13 +32,8 @@ app.use(
   })
 );
 
-app.use("/auth", authRoute);
-app.use("/patient", patientRoute);
-app.use("/doctor", doctorRoute);
-app.use("/schedule", ScheduleRoute);
-app.use("/record", RecordRoute);
-app.use("/prescription",PrescriptionsRoute)
-app.use("/invoices",InvoiceRoute);
+app.use("/v1", routes);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
