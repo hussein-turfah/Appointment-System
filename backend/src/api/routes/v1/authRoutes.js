fae51c0cee5dc2f4fe343500fb70dc0ceb35995d
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-10 01:45:11 +0200

    :boom: Added new functions

--- backend/src/api/routes/v1/authRoutes.js
+++ backend/src/api/routes/v1/authRoutes.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { login , forgotPassword } = require('../../controllers/authControllers');
+const {
+  login,
+  forgotPassword,
+  loggedIn,
+} = require("../../controllers/authControllers");
 
 // Route for user login
-router.post('/login', login);
-router.post('/forgetpassword',forgotPassword)
+router.post("/login", login);
+router.post("/forgetpassword", forgotPassword);
+router.get("/", loggedIn);
 
 module.exports = router;
