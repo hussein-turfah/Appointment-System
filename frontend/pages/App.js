e086300a0056b6757c0863282fdef756e50db4f7
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-10 04:13:23 +0200

    :boom: Add file: App.js

--- frontend/pages/App.js
+++ frontend/pages/App.js
@@ -1,9 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "../common/Navbar";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getAllDoctors } from "../actions/DoctorActions";
+import { getUser } from "../actions/UserActions";
 
 const MyApp = ({ Component, pageProps, domainName }) => {
   const router = useRouter();
@@ -11,20 +12,29 @@ const MyApp = ({ Component, pageProps, domainName }) => {
   const authPages = ["/login", "/register", "/forgot-password"];
   const isAuthPage = authPages.includes(router.pathname);
 
+  const [token, setToken] = useState(null);
 
   // If the user is not logged in and the page is not an auth page, redirect to the login page
   // useEffect(() => {
-  //   const token = localStorage.getItem("token");
   //   if (!token && !isAuthPage) {
   //     router.push("/login");
+  //   } 
+  //   else if (token && isAuthPage) {
+  //     router.push("/");
   //   }
-  // }, []);
+  // }, [router, isAuthPage]);
 
   useEffect(() => {
-    // get all doctors
-    dispatch(getAllDoctors());
-  }, [dispatch]);
+    if (token) {
+      dispatch(getAllDoctors());
+      // dispatch(getUser());
+    }
+  }, [dispatch, token]);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+  }, []);
 
   return (
     <div className="page">
@@ -36,7 +46,7 @@ const MyApp = ({ Component, pageProps, domainName }) => {
           animate={{ y: 0 }}
           transition={{ bounce: 0, type: "spring", duration: 0.8 }}
           className="pageContent"
-          style={{ top: !isAuthPage ? "3.5rem" : "0" }}
+          style={{ top: isAuthPage && "0" }}
         >
           <Component
             key={"pageContent"}
