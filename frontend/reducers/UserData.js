209813a7ae1d9c270e868fccd22909fc449ea2f7
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-10 09:47:11 +0200

    :wrench: Fix several issues

--- frontend/reducers/UserData.js
+++ frontend/reducers/UserData.js
@@ -1,16 +1,19 @@
 import { ACTIONS } from "../actions/UserActions";
 
 const initialState = {
-  loaded: false,
   data: {},
 };
 
 const UserData = (state = initialState, { type, data }) => {
   switch (type) {
-    case ACTIONS.SET_USER:
+    case ACTIONS.LOGIN_USER:
       return {
         ...state,
-        loaded: true,
+        data,
+      };
+    case ACTIONS.GET_USER:
+      return {
+        ...state,
         data,
       };
     default:
