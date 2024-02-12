9d6c6cce96ad4b081ed5c1b657d5ec75fa0d5d49
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-12 03:04:38 +0200

    :boom: Add new elements and files

--- frontend/common/CreateAppointmentMoodal/index.js
+++ frontend/common/CreateAppointmentMoodal/index.js
@@ -2,14 +2,13 @@ import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createAppointment } from "../../actions/AppointmentActions";
 import Dropdown from "../Dropdown";
-import { all } from "axios";
 
-export default function CreateAppointmentModal() {
+export default function CreateAppointmentModal({ active, setActive}) {
   const dispatch = useDispatch();
   const allDoctors = useSelector(({ DoctorData }) => DoctorData.allDoctors);
-
-  const [focus, setFocus] = useState(false);
-
+  const allPatients = useSelector(
+    ({ PatientData }) => PatientData.allPatients?.data
+  );
   const [formData, setFormData] = useState({
     doctor: allDoctors?.data[0]?.id,
     patient: "",
@@ -19,18 +18,16 @@ export default function CreateAppointmentModal() {
     reason: "",
   });
 
-  const create = useCallback(
-    (data) => {
-      dispatch(createAppointment(data));
-    },
-    [dispatch]
-  );
-  console.log;
+  const create = useCallback(() => {
+    dispatch(createAppointment(formData));
+  }, [dispatch, formData]);
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        create();
+        setActive(false);
       }}
     >
       <h3 className="mb-3 font-semibold">Appointment Details</h3>
@@ -38,32 +35,17 @@ export default function CreateAppointmentModal() {
         <label htmlFor="exampleFormControlInput1" className="form-label">
           Doctor
         </label>
-        {/* <select
-          className="form-select w-50 mx-auto mb-3 w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          aria-label="Default select example"
-          value={formData.doctor}
-          onChange={(e) => setFormData({ ...formData, doctor: e.target.value })}
-          onFocus={(e) => {
-            console.log(e.target.value);
-            setFocus(true);
-          }}
-        >
-          {allDoctors.data.map((doctor) => (
-            <option key={doctor.id} value={doctor.id}>
-              {doctor.firstName} {doctor.lastName}
-            </option>
-          ))}
-        </select> */}
         <Dropdown
           value={
-            formData.doctor ?
-            allDoctors.data?.filter(
-              (doctor) => doctor.id === formData?.doctor
-            )[0]?.firstName +
-            " " +
-            allDoctors.data.filter((doctor) => doctor.id === formData.doctor)[0]
-              ?.lastName
-            : ""
+            formData.doctor
+              ? allDoctors.data?.filter(
+                  (doctor) => doctor.id === formData?.doctor
+                )[0]?.firstName +
+                " " +
+                allDoctors.data.filter(
+                  (doctor) => doctor.id === formData.doctor
+                )[0]?.lastName
+              : ""
           }
           values={allDoctors.data.map(
             (doctor) => doctor.firstName + " " + doctor.lastName
@@ -77,7 +59,7 @@ export default function CreateAppointmentModal() {
             })
           }
         />
-        <div className="mb-3">
+        {/* <div className="mb-3">
           <label
             htmlFor="exampleFormControlInput1"
             className="form-label text-center"
@@ -91,13 +73,13 @@ export default function CreateAppointmentModal() {
             value={formData.date}
             onChange={(e) => setFormData({ ...formData, date: e.target.value })}
           />
-        </div>
+        </div> */}
         <div className="mb-3">
           <label htmlFor="exampleFormControlInput1" className="form-label">
             Start Time
           </label>
           <input
-            type="time"
+            type="datetime-local"
             className="form-control text-center w-50 mx-auto mb-3 w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
             id="exampleFormControlInput1"
             value={formData.start}
@@ -106,18 +88,18 @@ export default function CreateAppointmentModal() {
             }
           />
         </div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          End Time
-        </label>
-        <input
-          type="time"
-          className="form-control text-center w-50 mx-auto mb-3 w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-          id="exampleFormControlInput1"
-          value={formData.end}
-          onChange={(e) => setFormData({ ...formData, end: e.target.value })}
-        />
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            End Time
+          </label>
+          <input
+            type="datetime-local"
+            className="form-control text-center w-50 mx-auto mb-3 w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            id="exampleFormControlInput1"
+            value={formData.end}
+            onChange={(e) => setFormData({ ...formData, end: e.target.value })}
+          />
+        </div>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
@@ -130,6 +112,40 @@ export default function CreateAppointmentModal() {
           id="exampleFormControlTextarea1"
           rows="3"
         ></textarea>
+      </div>
+      <h4 className="mb-3 font-semibold">Patient Details</h4>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Patient Name
+        </label>
+        <Dropdown
+          value={
+            formData.patient
+              ? allPatients?.data?.filter(
+                  (patient) => patient.id === formData?.patient
+                )[0]?.firstName +
+                " " +
+                allPatients?.data.filter(
+                  (patient) => patient.id === formData.patient
+                )[0]?.lastName
+              : ""
+          }
+          values={
+            Array.isArray(allPatients?.data) &&
+            allPatients?.data?.map(
+              (patient) => patient.firstName + " " + patient.lastName
+            )
+          }
+          setValue={(value) =>
+            setFormData({
+              ...formData,
+              patient: allPatients.data.filter(
+                (patient) =>
+                  patient.firstName + " " + patient.lastName === value
+              )[0]?.id,
+            })
+          }
+        />
       </div>
       <button
         type="submit"
