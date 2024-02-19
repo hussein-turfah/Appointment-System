70193a460c1f95cc5c82e3803c85701a1542cec0
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-20 01:20:47 +0200

    :boom: Add new functions

--- frontend/pages/calendar/index.page.js
+++ frontend/pages/calendar/index.page.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./styles/index.module.scss";
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -22,6 +22,7 @@ import CreateUserModal from "../../common/CreateUserModal";
 export default function Calendar() {
   const dispatch = useDispatch();
   const router = useRouter();
+  const calendarRef = useRef(null);
 
   const [selectedDoctor, setSelectedDoctor] = useState("all");
   const [appointmentModal, setAppointmentModal] = useState(false);
@@ -31,6 +32,9 @@ export default function Calendar() {
   const user = useSelector(({ UserData }) => UserData.data);
   const allAppointments = useSelector(
     ({ AppointmentData }) => AppointmentData.allAppointments
+  );
+  const doctorAppointments = useSelector(
+    ({ AppointmentData }) => AppointmentData.appointmentsByDoctor
   );
   const allDoctors = useSelector(({ DoctorData }) => DoctorData.allDoctors);
 
@@ -48,12 +52,45 @@ export default function Calendar() {
     }
   }, [dispatch, user.role, user.type, user.id]);
 
+  // useEffect for double click and single click on event
   useEffect(() => {
-    console.log(isSelected.id);
+    const calendarApi = calendarRef.current.getApi();
+    let clickCnt = 0;
+
+    const handleEventClick = (info) => {
+      clickCnt++;
+
+      if (clickCnt === 1) {
+        setTimeout(() => {
+          if (clickCnt === 1) {
+            router.push(`/patients/${info.event.extendedProps.patient}`);
+          }
+          clickCnt = 0;
+        }, 400);
+      } else if (clickCnt === 2) {
+        setIsSelected(info.event);
+        clickCnt = 0;
+      }
+    };
+
+    calendarApi.on("eventClick", handleEventClick);
+
+    return () => {
+      calendarApi.off("eventClick", handleEventClick);
+    };
+  }, []);
+
+  // useEffect for setting isSelected to empty object after 2 seconds
+  useEffect(() => {
+    if (Object.keys(isSelected).length > 0) {
+      setTimeout(() => {
+        setIsSelected({});
+      }, 3000);
+    }
   }, [isSelected]);
 
   return (
-    <main>
+    <main className={styles.container}>
       {(user?.role === "admin" || user?.type === "secretary") && (
         <div className={styles.select}>
           <button
@@ -99,8 +136,10 @@ export default function Calendar() {
         </div>
       )}
       <Fullcalendar
+        ref={calendarRef}
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-        initialView="dayGridMonth"
+        // initialView="dayGridMonth"
+        initialView="timeGridWeek"
         headerToolbar={{
           start: "today prev,next",
           center: "title",
@@ -109,31 +148,56 @@ export default function Calendar() {
         height={"90vh"}
         events={
           user.type === "doctor"
-            ? Array.isArray(allAppointments?.data) &&
-              allAppointments?.data
-                ?.filter((appointment) => appointment.doctor === user.id)
-                .map((appointment) => ({
-                  ...appointment,
-                  eventColor: `#${appointment.doctor.slice(-6)}`,
-                }))
+            ? doctorAppointments.data
             : selectedDoctor === "all"
-            ? allAppointments?.data?.map((appointment) => ({
-                ...appointment,
-                eventColor: `#${appointment.doctor.slice(-6)}`,
-              }))
-            : allAppointments.data
-                .filter((appointment) => appointment.doctor === selectedDoctor)
-                .map((appointment) => ({
-                  ...appointment,
-                  eventColor: `#${appointment.doctor.slice(-6)}`,
-                }))
+            ? allAppointments.data
+            : allAppointments.data.filter(
+                (appointment) => appointment.doctor === selectedDoctor
+              )
         }
         editable={user?.role === "admin" || user?.type === "secretary"}
         droppable={user?.role === "admin" || user?.type === "secretary"}
         selectable={user?.role === "admin" || user?.type === "secretary"}
-        eventClick={(info) => {
-          setIsSelected(info.event);
+        slotDuration="00:15:00"
+        slotMinTime="08:00:00"
+        slotMaxTime="18:00:00"
+        eventContent={(e) => {
+          return (
+            <div className={styles.eventContent}>
+              {(user?.role === "admin" || user?.type === "secretary") && (
+                <p>
+                  Doctor: {e?.event?.extendedProps?.doctor?.firstName}{" "}
+                  {e?.event?.extendedProps?.doctor?.lastName}
+                </p>
+              )}
+              <p>
+                Patient: {e?.event?.extendedProps?.patient?.firstName}{" "}
+                {e?.event?.extendedProps?.patient?.lastName}
+              </p>
+              <p>Status: {e?.event?.extendedProps?.status}</p>
+              {console.log(e?.event?.extendedProps.doctor)}
+            </div>
+          );
         }}
+        eventBackgroundColor={
+          // user?.role === "admin" || user?.type === "secretary"?
+          (info) => {
+            if (info?.event?.extendedProps?.status === "created") {
+              return "blue";
+            } else if (info?.event?.extendedProps?.status === "done") {
+              return "green";
+            } else if (info?.event?.extendedProps?.status === "canceled") {
+              return "red";
+            } else if (info?.event?.extendedProps?.status === "absent") {
+              return "yellow";
+            } else if (info?.event?.extendedProps?.status === "rescheduled") {
+              return "purple";
+            } else {
+              return "black";
+            }
+          }
+          // : null
+        }
       />
       <Modal
         active={appointmentModal}
@@ -143,8 +207,8 @@ export default function Calendar() {
           <CreateAppointmentModal
             active={appointmentModal}
             setActive={setAppointmentModal}
-            selectedAppointment={isSelected.extendedProps}
-            appointmentId={isSelected.id}
+            selectedAppointment={isSelected?.extendedProps}
+            appointmentId={isSelected?.id}
             appointmentRange={{
               start: isSelected.startStr,
               end: isSelected.endStr,
