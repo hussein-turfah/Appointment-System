b8a5f2021956e75e9d7dbfe6a70760b4f898ee0b
Hussein Turfah <husseinturfah299@gmail.com>
2024-02-20 06:28:41 +0200

    :boom: Add new functions

--- backend/src/api/utils/createtoken.js
+++ backend/src/api/utils/createtoken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const createToken = (payload) =>
   jwt.sign({ userId: payload }, process.env.JWT_SECRET_KEY, {
-    expiresIn: '2h',
+    expiresIn: process.env.JWT_EXPIRATION,
   });
 
 module.exports = createToken;
